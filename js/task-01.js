2dcd592cae5ed457c51aea2525fe8261165e8dd5
Anastasiia Tkachenko <96151518+Anastasiia008@users.noreply.github.com>
2023-04-27 15:04:41 +0300

    all

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const categoriesEl = document.querySelectorAll('ul#categories li.item');
+
+console.log(`Number of categories: ${categoriesEl.length}`);
+
+categoriesEl.forEach(category => {
+    console.log(`Category: ${category.querySelector('h2').textContent}`);
+    console.log(`Elements: ${category.querySelectorAll('li').length}`);
+});
---
+++ js/task-09.js
@@ -0,0 +1,16 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const button = document.querySelector("button.change-color");
+const body = document.querySelector("body");
+const span = document.querySelector("span.color");
+span.textContent = "#fff";
+
+function onChangeColor() {
+  body.style.backgroundColor = span.textContent = getRandomHexColor();
+}
+
+button.addEventListener("click", onChangeColor);
